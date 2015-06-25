c9ca1f9eaef947b636dc3501baa9412f3bad3eca
Victor Jolissaint <vjolissaint@twitter.com>
2015-06-25 20:45:42 +0100

    Add node app, config file and update readme

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 Powering a bubble machine with Periscope's hearts
 
 ## Reqs
+* `Spark`/`Particle` device: https://www.particle.io
 * `node`
 
 ## Install
 ```
 git clone git@github.com:vjo/bubblescope.git bubblescope && cd bubblescope
 ```
+
+## Config
+Edit `config.json` file at the root of the project directory to set you Spark/Particle parameters.
 
 ### Node
 ```
---
+++ server/node/app.js
@@ -0,0 +1,124 @@
+#!/usr/bin/env node
+"use strict";
+
+var program = require('commander'),
+    querystring = require('querystring'),
+    chalk = require('chalk'),
+    fs = require('fs'),
+    https = require('https');
+
+// regex to check user passes proper periscope URL
+var PERISCOPE_URL_RE = /^https:\/\/www.periscope.tv\/w\/*/i,
+    CONFIG = {};
+
+// define color helper for terminal output
+var error = chalk.red,
+    info = chalk.blue,
+    action = chalk.magenta;
+
+program
+    .version('0.0.1')
+    .option('-u, --url <url>', 'periscope URL', null)
+    .parse(process.argv);
+
+/*
+ * Checking cli args
+ */
+if(!process.argv.slice(2).length) {
+    program.help();
+}
+if(!program.url) {
+    console.log(error('Error: you must specify a periscope URL.'));
+    program.help();
+}
+if(!program.url.match(PERISCOPE_URL_RE)) {
+    console.log(error('Error: wrong URL format'));
+    program.help();
+}
+
+var data = fs.readFileSync('./../../config.json');
+try {
+    CONFIG = JSON.parse(data);
+} catch (e) {
+    console.log(error('Error while parsing config.json file:'), e);
+}
+
+/**
+ * Get PubNub key to subscribe
+ * @param {String} token periscope token
+ */
+var getAccessPublic = function(token) {
+    return https.get('https://api.periscope.tv/api/v2/getAccessPublic?token=' + token, function(res) {
+        res.on('data', function (chunk) {
+            var d = JSON.parse(chunk);
+            var keys = {
+                auth_key      : d.auth_token,
+                subscribe_key : d.subscriber,
+                publish_key   : d.publisher,
+                channel       : d.channel
+            };
+            subscribeOnPubnub(keys);
+        });
+    }).on('error', function(e) {
+        console.log(error('Error getAccessPublic: ' + e.message));
+    });
+};
+
+/**
+ * Subscribe to PubNub API
+ * @param {Object} keys pubnub's keys
+ */
+var subscribeOnPubnub = function(keys){
+    var pubnub = require("pubnub")({
+        ssl           : true,
+        publish_key   : data.publish_key,
+        subscribe_key : data.subscribe_key
+    });
+    pubnub.subscribe({
+        channel    : data.channel,
+        auth_key   : data.auth_key,
+        callback   : function(message) {
+            if(message.type === 2){
+                console.log(action('Heart from ' + message.displayName));
+                sendHeart();
+            }
+        },
+        connect    : function(){ console.log(info('### Connected to PubNub!')); },
+        disconnect : function(){ console.log(info('### Disconnected from PubNub.')); },
+        error      : function (error) {
+            console.log(error('Error subscribe PubNub'));
+            console.log(JSON.stringify(error));
+        }
+    });
+};
+
+/**
+ * Send heart command to the spark/particle API
+ */
+var sendHeart = function() {
+    var post_data = querystring.stringify({});
+    var post_options = {
+        host: 'api.particle.io',
+        port: '443',
+        path: '/v1/devices/' + CONFIG.device_name + '/heart?access_token=' + CONFIG.access_token,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': post_data.length
+        }
+    };
+    var post_req = https.request(post_options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            if(chunk.error){
+                console.log(error('Error post: ' + chunk.error));
+            }
+        });
+    });
+    post_req.write(post_data);
+    post_req.end();
+};
+
+var extract = program.url.split('https://www.periscope.tv/w/')[1];
+var token = encodeURIComponent(extract);
+var data = getAccessPublic(token);
